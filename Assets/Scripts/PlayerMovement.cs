902852bf2608e99208c5e81d4ea486cffcc520aa
ItsAlexdro28 <alejandroj5025@gmail.com>
2022-05-15 17:06:13 -0500

    0.1 proyect creation
    create a new unity proyect

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 10;
+    public float jumpForce = 5;
+    // start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x =Input.GetAxis("Horizontal");
+        float y =Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+        Walk(dir);
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump(Vector2.up, false);
+        }
+
+    }
+    private void Walk(Vector2 dir)
+    {
+        rb.velocity=(new Vector2(dir.x*speed, rb.velocity.y));
+    }
+
+    private void Jump(Vector2 dir, bool wall)
+    {
+    rb.velocity = new Vector2(rb.velocity.x, 0);
+    rb.velocity += Vector2.up * jumpForce;
+    }
+                 
+                   
+}
